a4d64a2bbeaa2c50485e8a4fd9b32a3bb7496f13
Latsuj <justal.kevin@gmail.com>
2021-07-31 23:31:36 +0800

    [FEATURE] Testing the status and validation of fprm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import Experience_0001 from '@pages/formGroup/formGroup.component';
 import Experience_0002 from '@pages/formControl/formControl.component';
 import Experience_0003 from '@pages/nestedFormGroup/nestedFormGroup.component';
+import Experience_0004 from '@pages/validationForm/validationForm.component';
 
 const routes: Routes = [
+  { path: 'experience_0004', component: Experience_0004 },
   { path: 'experience_0003', component: Experience_0003 },
   { path: 'experience_0002', component: Experience_0002 },
   { path: 'experience_0001', component: Experience_0001 },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import Experience_0001 from '@pages/formGroup/formGroup.component';
 import Experience_0002 from '@pages/formControl/formControl.component';
 import Experience_0003 from '@pages/nestedFormGroup/nestedFormGroup.component';
+import Experience_0004 from '@pages/validationForm/validationForm.component';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
 
 @NgModule({
-  declarations: [AppComponent, Experience_0001, Experience_0002, Experience_0003],
+  declarations: [AppComponent, Experience_0001, Experience_0002, Experience_0003, Experience_0004],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
