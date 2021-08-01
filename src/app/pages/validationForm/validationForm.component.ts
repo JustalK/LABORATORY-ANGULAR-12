06f06bd27d3dc1d4c94bf0a3215823e7c801a11e
Latsuj <justal.kevin@gmail.com>
2021-08-02 00:01:26 +0800

    [FEATURE] Testing the validation and custom validation with css

--- src/app/pages/validationForm/validationForm.component.ts
+++ src/app/pages/validationForm/validationForm.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ValidationErrors, AbstractControl, ValidatorFn } from '@angular/forms';
+
+export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = nameRe.test(control.value);
+    return forbidden ? { forbiddenName: { value: control.value } } : null;
+  };
+}
+
+export function underAgeValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const isMinor = parseInt(control.value, 10) <= 18;
+    return isMinor ? { isMinor: { value: control.value } } : null;
+  };
+}
 
 @Component({
   selector: 'app-home',
@@ -7,10 +21,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export default class CustomComponent {
   identityForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
+    firstName: new FormControl('', [Validators.required, Validators.minLength(4), forbiddenNameValidator(/bob/i)]),
     lastName: new FormControl(''),
-    age: new FormControl(''),
+    age: new FormControl('', [Validators.required, underAgeValidator()]),
   });
+
+  get firstName() {
+    return this.identityForm.get('firstName');
+  }
+
+  get age() {
+    return this.identityForm.get('age');
+  }
 
   callingFunction() {
     console.log(this.identityForm.value);
