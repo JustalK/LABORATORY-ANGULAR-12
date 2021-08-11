cb2d97f35f6d5d804b2f08a22e91ee349741ff1c
Latsuj <justal.kevin@gmail.com>
2021-08-11 17:41:30 +0800

    [FEATURE] Adding the directive

--- src/app/pages/directives/directives.component.ts
+++ src/app/pages/directives/directives.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
 })
 export default class CustomComponent {
   books = [{ title: 'Title 1' }, { title: 'Title 2' }];
+
+  isVisible = false;
+
+  cssClasses: string[] = [];
+
+  handleIsVisible() {
+    this.isVisible = !this.isVisible;
+  }
+
+  changeClasses() {
+    if (this.cssClasses.includes('red')) {
+      this.cssClasses = [];
+    } else {
+      this.cssClasses.push('red');
+    }
+  }
 }
